bd892a7a5494daac9af1b4efc9b9b6f4a77dc185
Loic2888 <12188@holbertonstudents.com>
2026-02-24 10:13:15 +0100

    feat: creat a class amenity

---
+++ part2/hbnb/app/models/amenity.py
@@ -0,0 +1,8 @@
+from base_models import BaseModel
+
+
+class Amenity(BaseModel):
+    def __init__(self, name, description):
+        super().__init__()
+        self.name = name
+        self.description = description
