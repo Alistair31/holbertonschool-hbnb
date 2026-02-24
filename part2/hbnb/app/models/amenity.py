f11918e1ae41d37e7c8e08748c52e3f5bd8f9243
Loic2888 <12188@holbertonstudents.com>
2026-02-24 13:51:34 +0100

    feat: add the validation of amenity

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -2,7 +2,23 @@ from base_models import BaseModel
 
 
 class Amenity(BaseModel):
-    def __init__(self, name, description):
+    def __init__(self, name: str, description: str):
         super().__init__()
-        self.name = name
-        self.description = description
+
+        if not isinstance(name, str) or not name.strip():
+            raise ValueError("Name must be a non-empty string")
+        if not isinstance(description, str) or not description.strip():
+            raise ValueError("Description must be a non-empty string")
+
+        self.name: str = name
+        self.description: str = description
+
+    @staticmethod
+    def verification_name(name: str) -> bool:
+        """Vérifie si name est une chaîne de caractères non vide."""
+        return isinstance(name, str) and bool(name.strip())
+
+    @staticmethod
+    def verification_description(description: str) -> bool:
+        """Vérifie si description est une chaîne de caractères non vide."""
+        return isinstance(description, str) and bool(description.strip())
