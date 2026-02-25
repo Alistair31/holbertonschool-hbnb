06887e04aa617e6e44cb4994829280c67520e3cf
Loic2888 <12188@holbertonstudents.com>
2026-02-25 14:34:00 +0100

    feat: implement amenities management with CRUD operations and update app structure

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -28,3 +28,28 @@ class HBnBFacade:
         self.user_repo.update(user_id, user_data)
 
         return user
+
+    def create_amenity(self, amenity_data):
+        # logic to create an amenity
+        from app.models.amenity import Amenity
+        new_amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(new_amenity)
+        return new_amenity
+
+    def get_amenity(self, amenity_id):
+        # logic to retrieve an amenity by ID
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        # logic to retrieve all amenities
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        # logic to update an amenity
+        self.amenity_repo.update(amenity_id, amenity_data)
+        return self.amenity_repo.get(amenity_id)
+
+    def get_amenity_by_name(self, name):
+        # logic to retrieve an amenity by name
+        all_amenities = self.amenity_repo.get_all()
+        return next((a for a in all_amenities if a.name == name), None)
