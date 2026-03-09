dce749912f3a9f1b7b3184be1e3dc69bae619480
Loic2888 <12188@holbertonstudents.com>
2026-03-09 18:37:34 +0100

    feat: add the create app objet of the task 0, and the pasword hash of the task 1

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -4,18 +4,24 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
+from flask_bcrypt import Bcrypt
 
 
-def create_app():
+bcrypt = Bcrypt()
+
+
+def create_app(config_class="app.config.DevelopmentConfig"):
     app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    bcrypt.init_app(app)
+
     api = Api(app, version='1.0', title='HBnB API',
               description='HBnB Application API', doc='/')
 
-    # Placeholder for API namespaces (endpoints will be added later)
-    # Additional namespaces for places,
-    # reviews, and amenities will be added later
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+
     return app
