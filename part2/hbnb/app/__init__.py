98e5627d5cde62f5f571ee721201ef1d01debbe0
Alistair31 <gabikill@hotmail.fr>
2026-03-10 10:51:55 +0100

    feat: Add JWT token expiration handling and password hashing

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -4,6 +4,7 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.auth import api as auth_ns
 from flask_bcrypt import Bcrypt
 
 
@@ -23,5 +24,9 @@ def create_app(config_class="app.config.DevelopmentConfig"):
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
-
+    api.add_namespace(auth_ns, path='/api/v1/auth')
     return app
+
+
+def expired_token_callback(jwt_header, jwt_payload):
+    return {"error": "Token has expired"}, 401
